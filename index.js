e2b0cb171775d8e25abd996b0b0289652a3e532a
Derek Enlow <derekenlow@gmail.com>
2025-02-18 21:06:03 -0700

    giveaway scraping and entering implemented

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ async function loadCookies(page) {
   await page.setCookie(...cookies);
 }
 
+// Enter a giveaway.
+async function enter(page, url) {
+  await page.goto('https://www.steamgifts.com' + url);
+  await page.locator('.sidebar__entry-insert').click();
+  // const title = await page.title();
+  // console.log(title);
+}
+
 (async () => {
   const browser = await puppeteer.launch(
     // {headless: false}
@@ -16,5 +24,13 @@ async function loadCookies(page) {
   await page.goto("https://www.steamgifts.com/");
   await loadCookies(page);
   await page.goto("https://www.steamgifts.com/giveaways/search?type=wishlist");
+  const selector = '.page__heading + div .giveaway__row-inner-wrap:not(.is-faded) .giveaway__heading__name';
+  const urls = await page.$$eval(selector, headers => {
+    return headers.map(header => header.getAttribute('href'));
+  })
+  // console.log(urls)
+  for (const url of urls) {
+    await enter(page, url);
+  }
   await browser.close();
 })();
